008f41e7c425c80d996e8ecef69c2c18568af1d6
shivam <shivamtrivedi313@gmail.com>
2022-10-15 12:45:38 +0530

    payment4

---
+++ payment.js
@@ -0,0 +1,28 @@
+console.log("sdfsdf")
+
+document.querySelector("form").addEventListener("submit", getData)
+
+function getData() {
+    event.preventDefault()
+
+    let cn = document.getElementById("cname").value
+    let cnu = document.getElementById("ccnum").value
+    let expm = document.getElementById("expmonth").value
+    let expy = document.getElementById("expyear").value
+    let cv = document.getElementById("cvv").value
+
+
+    // if (cn === "" || cnu === "" || expm === "" || expy === "" || cv === "") {
+    //     alert("Enter all the fields")
+    // } else {
+    //     alert(Congrats!! Your order is placeed)
+    //     //     window.location.href = './payment.html'
+    // }
+
+    if (cn == "" || expm === "" || cnu === "" || expy === "" || cv === "") {
+        alert("Enter all the fields")
+    } else {
+        alert("Congrats !! Your order is placed .")
+        //window.location.href = './ind.html'
+    }
+}
